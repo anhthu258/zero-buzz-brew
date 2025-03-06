60a11151c51cfa6d97d3eed562856555e57cefc5
anhthu258 <k.anhthu02@gmail.com>
2025-03-06 12:20:45 +0100

    smth aint right
    >:C

---
+++ javascript/app.js
@@ -0,0 +1,13 @@
+let iconKurv = document.querySelector('.iconKurv');
+let cart = document.querySelector('.cart');
+let close = document.querySelector('.close');
+
+iconKurv.addEventListener('click', () => {
+  if(cart.style.right === '-100%'){
+    cart.style.right = '0';
+    container.style.transform = 'translateX(-400px)';
+    } else{
+        cart.style.right = '-100%';
+        container.style.transform = 'translateX(0)'
+    }
+})
