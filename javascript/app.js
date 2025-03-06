0cfd2f037d7fe0b3c8abb9dbb1d5508fd37940db
anhthu258 <k.anhthu02@gmail.com>
2025-03-06 12:32:36 +0100

    am a dumbass
    glemte at linke js filen til html

--- javascript/app.js
+++ javascript/app.js
@@ -4,10 +4,15 @@ let close = document.querySelector('.close');
 
 iconKurv.addEventListener('click', () => {
   if(cart.style.right === '-100%'){
-    cart.style.right = '0';
-    container.style.transform = 'translateX(-400px)';
+        cart.style.right = '0';
+        container.style.transform = 'translateX(-400px)';
     } else{
         cart.style.right = '-100%';
         container.style.transform = 'translateX(0)'
     }
 })
+
+close.addEventListener('click', () => {
+    cart.style.right = '-100%';
+        container.style.transform = 'translateX(0)';
+    })
